4f2d6190a8d1b8ca86f9b2a6bb93d1abd32f1b50
Tomoaki Yagishita <tyagishi@gmail.com>
2023-03-22 20:49:09 +0900

    missed to commit updated tests

--- Tests/SDSSwiftExtensionTests/PairIterator_Tests.swift
+++ Tests/SDSSwiftExtensionTests/PairIterator_Tests.swift
@@ -42,7 +42,7 @@ final class PairIterator_Tests: XCTestCase {
         XCTAssertNil(iterator.next())
     }
 
-    func test_makePairIterator() async throws {
+    func test_makePairIterator_Array() async throws {
         let array = [1,2,3,4]
         var iterator = array.makePairIterator()
         let (c1,n1) = try XCTUnwrap(iterator.next())
@@ -63,5 +63,28 @@ final class PairIterator_Tests: XCTestCase {
 
         XCTAssertNil(iterator.next())
     }
+    func test_makePairIterator_Set() async throws {
+        let array: Set<Int> = [1,2,3,4]
+        var iterator = array.makePairIterator()
+        let (c1,n1) = try XCTUnwrap(iterator.next())
+        XCTAssertNotNil(c1)
+        XCTAssertNotNil(n1)
 
+        let (c2,n2) = try XCTUnwrap(iterator.next())
+        XCTAssertNotNil(c2)
+        XCTAssertNotNil(n2)
+        XCTAssertEqual(n1, c2)
+
+        let (c3,n3) = try XCTUnwrap(iterator.next())
+        XCTAssertNotNil(c3)
+        XCTAssertNotNil(n3)
+        XCTAssertEqual(n2, c3)
+
+        let (c4,n4) = try XCTUnwrap(iterator.next())
+        XCTAssertNotNil(c4)
+        XCTAssertNil(n4)
+        XCTAssertEqual(n3, c4)
+
+        XCTAssertNil(iterator.next())
+    }
 }
