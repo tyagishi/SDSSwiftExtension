3f4af321497d356780699c9d974dc35863162435
tyagishi <tyagishi@gmail.com>
2024-12-02 21:57:48 +0900

    F - add ClosedRange.extend

--- Sources/SDSSwiftExtension/ClosedRange+Extension.swift
+++ Sources/SDSSwiftExtension/ClosedRange+Extension.swift
@@ -1,8 +1,19 @@
 //
-//  File.swift
+//  ClosedRange+Extension.swift
 //  SDSSwiftExtension
 //
 //  Created by Tomoaki Yagishita on 2024/12/02.
 //
 
 import Foundation
+
+extension ClosedRange {
+    public func extend(to newValue: Bound) -> ClosedRange {
+        if newValue < self.lowerBound {
+            return newValue...self.upperBound
+        } else if self.upperBound < newValue {
+            return self.lowerBound...newValue
+        }
+        return self
+    }
+}
